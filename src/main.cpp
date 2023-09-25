74a800cdb88fc81b378d93b37155260bbf36e438
FlayaN <hannes.feldt@gmail.com>
2023-09-25 23:43:24 +0200

    refactor: use add_commonlibsf_plugin

--- src/main.cpp
+++ src/main.cpp
@@ -11,27 +11,6 @@ void MessageHandler(SFSE::MessagingInterface::Message* a_message)
 	}
 }
 
-#undef USE_ADDR_LIB
-
-DLLEXPORT constinit auto SFSEPlugin_Version = []() noexcept {
-	SFSE::PluginVersionData data{};
-
-	data.PluginVersion(Plugin::VERSION);
-	data.PluginName(Plugin::NAME);
-	data.AuthorName(Plugin::AUTHOR);
-	data.HasNoStructUse(true);
-
-#ifdef USE_ADDR_LIB
-	data.UsesAddressLibrary(true);
-	data.MinimumRequiredXSEVersion({ SFSE::RUNTIME_SF_1_7_23 });
-#else
-	data.UsesSigScanning(true);
-	data.CompatibleVersions({ SFSE::RUNTIME_SF_1_7_29 });
-#endif  // USE_ADDR_LIB
-
-	return data;
-}();
-
 DLLEXPORT bool SFSEAPI SFSEPlugin_Load(const SFSE::LoadInterface* a_sfse)
 {
 #ifndef NDEBUG
@@ -39,8 +18,8 @@ DLLEXPORT bool SFSEAPI SFSEPlugin_Load(const SFSE::LoadInterface* a_sfse)
 #endif
 	SFSE::Init(a_sfse);
 
-	auto version = Plugin::VERSION.string();
-	logger::info("Loaded {} {}", Plugin::NAME, version);
+	auto version = Plugin::Version.string();
+	logger::info("Loaded {} {}", Plugin::Name, version);
 
 	const auto messaging = SFSE::GetMessagingInterface();
 	messaging->RegisterListener(MessageHandler);
